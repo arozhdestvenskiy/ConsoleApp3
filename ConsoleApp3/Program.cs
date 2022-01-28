43a2c2849375b395265fbbd8e699bc6cb8c6a732
asxmas <asxmas@gmail.com>
2022-01-28 13:51:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("start prog");
+        NFCReader NFC = new NFCReader();
+        Console.WriteLine(NFC.Connect());
+        Console.WriteLine(NFC.WriteBlock("try to write", "2"));
+        
+    }
+
+}
